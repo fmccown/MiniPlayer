4a6f63fe0928374d44c59267ea85f847e85f99ed
Frank McCown <fmccown@harding.edu>
2020-10-20 09:28:22 -0500

    Add project

---
+++ MiniPlayerWpf/MainWindow.xaml.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MiniPlayerWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MusicLib musicLib;
+        private MediaPlayer mediaPlayer;        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mediaPlayer = new MediaPlayer();
+
+            try
+            {
+                // Tell the lib where the exe resides
+                musicLib = new MusicLib(); 
+            }
+            catch (Exception e)
+            {
+                DisplayError("Error loading file: " + e.Message);
+                return;
+            }
+
+            //musicLib.PrintAllTables();
+            
+            // Put the ids to a ObservableCollection which has a Remove method for use later.
+            // The UI will update itself automatically if any changes are made to this collection.
+            ObservableCollection<string> items = new ObservableCollection<string>(musicLib.SongIds);     
+
+            // Bind the song IDs to the combo box
+            songIdComboBox.ItemsSource = items;
+            
+            // Select the first item
+            if (songIdComboBox.Items.Count > 0)
+            {
+                songIdComboBox.SelectedItem = songIdComboBox.Items[0];
+                deleteButton.IsEnabled = true;
+            }
+        }
+
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.FileName = ""; 
+            openFileDialog.DefaultExt = "*.wma;*.wav;*mp3";
+            openFileDialog.Filter = "Media files|*.mp3;*.m4a;*.wma;*.wav|MP3 (*.mp3)|*.mp3|M4A (*.m4a)|*.m4a|Windows Media Audio (*.wma)|*.wma|Wave files (*.wav)|*.wav|All files|*.*";
+
+            // Show open file dialog box
+            bool? result = openFileDialog.ShowDialog();
+
+            // Load the selected song
+            if (result == true)
+            {
+                songIdComboBox.IsEnabled = false;
+                Song s = GetSongDetails(openFileDialog.FileName);
+                if (s != null)
+                {
+                    titleTextBox.Text = s.Title;
+                    artistTextBox.Text = s.Artist;
+                    albumTextBox.Text = s.Album;
+                    genreTextBox.Text = s.Genre;
+                    lengthTextBox.Text = s.Length;
+                    filenameTextBox.Text = s.Filename;
+                    mediaPlayer.Open(new Uri(s.Filename));
+                    addButton.IsEnabled = true;
+                }
+            }
+        }
+
+        private Song GetSongDetails(string filename)
+        {
+            Song s = null;
+
+            try
+            {
+                // PM> Install-Package taglib
+                // http://stackoverflow.com/questions/1750464/how-to-read-and-write-id3-tags-to-an-mp3-in-c
+                TagLib.File file = TagLib.File.Create(filename);
+
+                s = new Song
+                {
+                    Title = file.Tag.Title,
+                    Artist = file.Tag.AlbumArtists.Length > 0 ? file.Tag.AlbumArtists[0] : "",
+                    Album = file.Tag.Album,
+                    Genre = file.Tag.Genres.Length > 0 ? file.Tag.Genres[0] : "",
+                    Length = file.Properties.Duration.Minutes + ":" + file.Properties.Duration.Seconds,
+                    Filename = filename
+                };
+
+                return s;
+            }
+            catch (TagLib.UnsupportedFormatException)
+            {
+                DisplayError("You did not select a valid song file.");
+            }
+            catch (Exception ex)
+            {
+                DisplayError(ex.Message);                
+            }
+                
+            return s;            
+        }
+
+        private void DisplayError(string errorMessage)
+        {
+            MessageBox.Show(errorMessage, "MiniPlayer", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Adding song");
+
+            // Add the selected file to the music library
+            Song s = new Song
+            {
+                Title = titleTextBox.Text,
+                Artist = artistTextBox.Text,
+                Album = albumTextBox.Text,
+                Genre = genreTextBox.Text,
+                Length = lengthTextBox.Text,
+                Filename = filenameTextBox.Text
+            };
+
+            string id = musicLib.AddSong(s).ToString();
+            Console.WriteLine("id = " + id);
+
+            // Add the song ID to the combo box
+            songIdComboBox.IsEnabled = true;            
+            (songIdComboBox.ItemsSource as ObservableCollection<string>).Add(id);
+            songIdComboBox.SelectedIndex = songIdComboBox.Items.Count - 1;
+
+            // There is at least one song that can be deleted
+            deleteButton.IsEnabled = true;
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string songId = songIdComboBox.SelectedItem.ToString();
+            Console.WriteLine("Updating song " + songId);
+               
+            Song s = new Song        
+            {
+                Title = titleTextBox.Text,
+                Artist = artistTextBox.Text,
+                Album = albumTextBox.Text,
+                Genre = genreTextBox.Text,
+                Length = lengthTextBox.Text,
+                Filename = filenameTextBox.Text
+            };
+
+            if (musicLib.UpdateSong(Convert.ToInt32(songId), s))
+            {
+                updateButton.IsEnabled = false;
+            }
+            else
+            {
+                DisplayError("There was a problem updating this song.");
+            }
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete this song?", "MiniPlayer", 
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                string songId = songIdComboBox.SelectedItem.ToString();
+                Console.WriteLine("Deleting song " + songId);
+
+                if (musicLib.DeleteSong(Convert.ToInt32(songId)))
+                {
+                    // Remove the song from the list box and select the next item
+                    (songIdComboBox.ItemsSource as ObservableCollection<string>).Remove(
+                        songIdComboBox.SelectedItem.ToString());
+                    if (songIdComboBox.Items.Count > 0)
+                        songIdComboBox.SelectedItem = songIdComboBox.Items[0];
+                    else
+                    {
+                        // No more songs to display
+                        deleteButton.IsEnabled = false;
+                        titleTextBox.Text = "";
+                        artistTextBox.Text = "";
+                        albumTextBox.Text = "";
+                        genreTextBox.Text = "";
+                        lengthTextBox.Text = "";
+                        filenameTextBox.Text = "";
+                    }
+                }
+                else
+                    DisplayError("There was a problem deleting this song.");                
+            }
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Play();
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+
+        private void showDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            musicLib.PrintAllTables();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            musicLib.Save();
+        }
+
+        private void songIdComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Display the selected song
+            if (songIdComboBox.SelectedItem != null)
+            {
+                Console.WriteLine("Load song " + songIdComboBox.SelectedItem);
+                int songId = Convert.ToInt32(songIdComboBox.SelectedItem);
+                Song s = musicLib.GetSong(songId);
+                titleTextBox.Text = s.Title;
+                artistTextBox.Text = s.Artist;
+                albumTextBox.Text = s.Album;
+                genreTextBox.Text = s.Genre;
+                lengthTextBox.Text = s.Length;
+                filenameTextBox.Text = s.Filename;
+                mediaPlayer.Open(new Uri(filenameTextBox.Text));
+                updateButton.IsEnabled = false;
+            }
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            updateButton.IsEnabled = true;
+        }
+    }
+}
---
+++ MusicLibTests/MusicLibUnitTest.cs
@@ -0,0 +1,193 @@
+﻿using MiniPlayerWpf;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using OpenQA.Selenium.Appium;
+using System.IO;
+
+namespace MusicLibTests
+{
+    [TestClass]
+    public class MusicLibUnitTest
+    {
+        // Note: Id is next Id that is auto-incremented 
+        private Song defaultSong = new Song
+        {
+            Id = 9,
+            Artist = "Bob",
+            Album = "Fire",
+            Filename = "test.mp3",
+            Genre = "cool",
+            Length = "123",
+            Title = "Best Song"
+        };
+
+        [TestMethod]
+        public void TestSongIds()
+        {
+            MusicLib musicLib = new MusicLib();
+            var songIds = new List<string>(musicLib.SongIds);
+
+            // Make sure there are 7 IDs
+            Assert.AreEqual(7, songIds.Count);
+
+            // Make sure all IDs but 4 are present
+            int idNum = 1;
+            foreach (string id in songIds)
+            {
+                // There is no ID 4
+                if (idNum == 4)
+                    idNum++;
+
+                Assert.AreEqual(idNum.ToString(), id);
+                idNum++;
+            }
+        }
+
+        [TestMethod]
+        public void AddSongTest()
+        {
+            MusicLib musicLib = new MusicLib();
+
+            // ID auto-increments
+            int expectedId = 9;
+            int actualId = musicLib.AddSong(defaultSong);
+            Assert.AreEqual(expectedId, actualId, "ID of added song was unexpectedly " + actualId);
+
+            // See if we can get back the song that was just added
+            Song song = musicLib.GetSong(expectedId);
+            Assert.AreEqual(defaultSong, song, "Got back unexpected song: " + song);
+        }
+
+        [TestMethod]
+        public void DeleteSongTest()
+        {
+            MusicLib musicLib = new MusicLib();
+
+            // Delete a song that already exists
+            int songId = 8;
+            bool songDeleted = musicLib.DeleteSong(songId);
+            Assert.IsTrue(songDeleted, "Song should have been deleted");
+
+            // Verify the song is not in the library anymore
+            Song s = musicLib.GetSong(songId);
+            Assert.IsNull(s, "Returned song should be null because it doesn't exist");
+        }
+
+        [TestMethod]
+        public void DeleteMissingSongTest()
+        {
+            MusicLib musicLib = new MusicLib();
+
+            // Attempt to delete a song that does not exist
+            int songId = 111;
+            bool songDeleted = musicLib.DeleteSong(songId);
+            Assert.IsFalse(songDeleted, "Non-existing song should not have been deleted");
+        }
+
+        [TestMethod]
+        public void GetSongTest()
+        {
+            MusicLib musicLib = new MusicLib();
+
+            // Add the default song and then retrieve it
+            int songId = musicLib.AddSong(defaultSong);
+            Song song = musicLib.GetSong(songId);
+            Assert.AreEqual(defaultSong, song);            
+        }
+
+        [TestMethod]
+        public void GetMissingSongTest()
+        {
+            MusicLib musicLib = new MusicLib();
+
+            // Get a song that doesn't exist
+            int songId = 111;
+            Song song = musicLib.GetSong(songId);
+            Assert.IsNull(song);
+        }
+
+        [TestMethod]
+        public void UpdateSongTest()
+        {
+            MusicLib musicLib = new MusicLib();
+
+            // Update everything except the ID
+            int songId = musicLib.AddSong(defaultSong);
+            Song updatedSong = new Song
+            {
+                Id = songId,
+                Title = "Some title",
+                Album = "Some album",
+                Artist = "Some artist",
+                Filename = "Some filename",
+                Genre = "Some genre",
+                Length = "Some length"
+            };
+
+            bool isUpdated = musicLib.UpdateSong(songId, updatedSong);
+            Assert.IsTrue(isUpdated, "Update should have worked; got back " + isUpdated);
+
+            // Make sure the song's title was really changed
+            Song s = musicLib.GetSong(songId);
+            Assert.AreEqual(updatedSong, s, "Song not updated properly");
+        }
+
+        [TestMethod]
+        public void UpdateMissingSongTest()
+        {
+            MusicLib musicLib = new MusicLib();
+
+            // Try to update a song with a bad ID
+            int songId = 111;
+            bool isUpdated = musicLib.UpdateSong(songId, defaultSong);
+            Assert.IsFalse(isUpdated, "Update should NOT have worked; got back " + isUpdated);
+        }
+
+        [TestMethod]
+        public void TestAlarm()
+        {
+            // Launch the Alarms & Clock app
+            //DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            //appCapabilities.SetCapability("app", "Microsoft.WindowsAlarms_8wekyb3d8bbwe!App");
+            AppiumOptions options = new AppiumOptions();
+            options.AddAdditionalCapability("app", "Microsoft.WindowsAlarms_8wekyb3d8bbwe!App");
+            var alarmClockSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+
+            // Use the session to control the app
+            alarmClockSession.FindElementByAccessibilityId("AddAlarmButton").Click();
+            alarmClockSession.FindElementByAccessibilityId("AlarmNameTextBox").Click();
+            alarmClockSession.FindElementByAccessibilityId("AlarmNameTextBox").Clear();
+            alarmClockSession.FindElementByAccessibilityId("AlarmNameTextBox").SendKeys("Test alarm");
+        }
+
+        [TestMethod]
+        public void TestUi()
+        {
+            // Launch the Alarms & Clock app
+            //DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            //appCapabilities.SetCapability("app", "Microsoft.WindowsAlarms_8wekyb3d8bbwe!App");
+            string currentDir = @"C:\Users\fmccown\Documents\Visual Studio 2019\Projects\MiniPlayerImprovedWpf\MiniPlayerWpf\bin\Debug\"; 
+            string appPath = currentDir + @"\MiniPlayerWpf.exe";
+
+            //Directory.SetCurrentDirectory(currentDir);
+
+            AppiumOptions options = new AppiumOptions();
+            options.AddAdditionalCapability("app", appPath);
+            options.AddAdditionalCapability("appWorkingDir", currentDir);
+            var session = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+
+            // Not sure how to select specific song
+            //session.FindElementByAccessibilityId("songIdComboBox").Click();
+            session.FindElementByAccessibilityId("titleTextBox").Clear();
+            session.FindElementByAccessibilityId("titleTextBox").SendKeys("My Title");
+            session.FindElementByAccessibilityId("addButton").Click();
+
+            var songId = session.FindElementByAccessibilityId("songIdComboBox").Text;
+            Assert.AreEqual(songId, "10");
+        }
+
+    }
+}
